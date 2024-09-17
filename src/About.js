b117aae77583474fda9a91c586f39ce0fd621f2c
255AM <lanecw@gmail.com>
2024-09-17 19:19:20 -0400

    fix robot fuckups

--- src/About.js
+++ src/About.js
@@ -9,16 +9,17 @@ const About = () => (
     </div>
     <div className="about-text">
       <p>
-        I’m an experienced reporter, editor, and newsroom leader who excels at elevating reporters’ work to serve readers and listeners.
-        During my time at nonprofit newsrooms, working in both radio and digital media, my work has been honored with an
+      I’m an experienced reporter, editor and newsroom leader who excels at elevating reporters’ work to serve readers and listeners. 
+      During my time at nonprofit newsrooms, working in both radio and digital media, my work has been honored with an
         <a href="https://www.spj.org/sdxa13.asp" target="_blank" rel="noopener noreferrer"> SPJ Sigma Delta Chi award,</a>
         regional and national Edward R. Murrow awards, and a
         <a href="https://peabodyawards.com/award-profile/the-popes-long-con/" target="_blank" rel="noopener noreferrer"> Peabody Award.</a>
       </p>
       <p>
-        In 2024-2025, I’m interested in working with newsrooms to optimize their editorial processes, consult on topics from coverage plan creation to newsroom management,
-        and am available to advise and assist on special projects, including around the election.
-        I am also focused on shrinking news deserts across the country and improving the sustainability of journalism startups.
+      In 2024-2025, I’m interested in working with newsrooms to optimize their editorial processes, consult on topics from coverage plan creation to newsroom management, 
+      and am available to advise and pinch hit on special projects, including around the election. As someone who has spent their entire career in local news, 
+      I’m also interested in working to shrink news deserts across the country — and crucially, studying how to improve the sustainability of the 
+      newest crop of journalism startups by researching and generating actionable steps for both reporters and editors to take when it comes to newsroom culture.
       </p>
     </div>
   </div>
--- src/WhatIveDone.js
+++ src/WhatIveDone.js
@@ -5,23 +5,30 @@ const WhatIveDone = () => (
     <div className="about-text">
       <h2>What I've Done</h2>
       <p>
-        As a <strong>reporter</strong>, I built environment and energy beats at two public radio stations. In two coal-reliant regions, my reporting highlighted the
-        effects of lawmakers’ inactions on coalfield residents.
+        As a <strong>reporter</strong>, I built environment and energy beats at two public radio stations: West Virginia Public Broadcasting and Louisville Public Media. In two places where the 
+        energy and economy was reliant on coal, my years of reporting revealed the real-world effects of lawmakers’ inactions on coalfield residents.
       </p>
       <p>
-        As an <strong>editor</strong>, I’ve worked with students, staff reporters, and freelancers from concept to publication, across news, podcasts, and investigations.
+        As an <strong>editor</strong>, I’ve worked with students, staff reporters and freelancers from concept to publication on everything from spot news to podcasts to investigations.
       </p>
       <p>
-        As a <strong>newsroom leader</strong>, I led large teams at Louisville Public Media, launching a weekly show, creating award-winning podcasts, and managing
-        coverage of a historic pandemic and racial justice protests.
+        As a <strong>newsroom leader</strong>,  I led the large local, regional and investigative team at 
+        Louisville Public Media. During my time as Director of News and Programming, my team launched a weekly show, created multiple award-winning podcasts and catapulted 
+        ourselves into becoming a main source of news for local residents through our coverage of a historic pandemic and racial justice protests. Later, at Mountain State Spotlight 
+        in West Virginia, I built an audience and membership program from the ground up, crafted coverage plans, launched new products and managed the reporting staff.
       </p>
       <p>
-        <strong>Currently</strong>, since June 2024, I’ve been working with newsrooms across the country to help them create local voter guides and access free election resources through a grant from the Knight Foundation. I’m an editor-at-large for Mountain State Spotlight, digital managing editor for 100 Days in Appalachia, and a contract editor for the Future Today Institute.
+        <strong>Currently</strong>, I’m wearing lots of hats. Since June 2024, I’ve been working with newsrooms across the country to help them create local 
+        voter guides and access free election resources through a grant from the John S. and James L. Knight Foundation. (Read some of the coverage of that 
+        initiative <a href="https://rjionline.org/news/key-questions-to-consider-when-building-voter-guides/" target="_blank" rel="noopener noreferrer" className="btn">here</a> and 
+        <a href="https://www.ire.org/product/ire-journal-q3-2024/" target="_blank" rel="noopener noreferrer" className="btn">here</a>). I’m an editor-at-large for <a href="https://mountainstatespotlight.org/" target="_blank" rel="noopener noreferrer" className="btn">Mountain State Spotlight,</a> 
+        and the digital managing editor for <a href="https://www.100daysinappalachia.com/" target="_blank" rel="noopener noreferrer" className="btn">100 Days in Appalachia. </a>
+        I’m also a contract editor for <a href="https://futuretodayinstitute.com/" target="_blank" rel="noopener noreferrer" className="btn">Future Today Institute,</a> working to make sure their annual Trend Report is clear, accurate, and accessible to readers around the world.
       </p>
       <p>
         I live outside Louisville, Kentucky with my husband and two sons.
       </p>
-      <p><a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="btn">Click here for my resume</a></p>
+      <p><a href="public/resume.pdf" target="_blank" rel="noopener noreferrer" className="btn">Click here for my resume</a></p>
     </div>
   </div>
 );
